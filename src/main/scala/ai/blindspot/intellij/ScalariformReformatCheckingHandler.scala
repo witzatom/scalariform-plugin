e4c3770df0862cec739a1acb9293e412a4a66a6f
Tomas Witzany <tomas.witzany@blindspot.ai>
2019-05-17 11:41:13 +0200

    use different state than the default reformat

--- src/main/scala/ai/blindspot/intellij/ScalariformReformatCheckingHandler.scala
+++ src/main/scala/ai/blindspot/intellij/ScalariformReformatCheckingHandler.scala
@@ -2,9 +2,10 @@ package ai.blindspot.intellij
 
 import java.awt.GridLayout
 
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.{DumbService, Project}
-import com.intellij.openapi.vcs.{CheckinProjectPanel, VcsConfiguration}
+import com.intellij.openapi.vcs.CheckinProjectPanel
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.checkin.{CheckinHandler, CheckinMetaHandler}
 import com.intellij.openapi.vcs.ui.RefreshableOnComponent
@@ -15,6 +16,8 @@ import scala.collection.JavaConverters._
 
 class ScalariformReformatCheckingHandler(project: Project, projectPanel: CheckinProjectPanel)
   extends CheckinHandler with CheckinMetaHandler {
+  val REFORMAT_BEFORE_PROJECT_COMMIT = "scalariform_formatter_reformat"
+
   override def getBeforeCheckinConfigurationPanel: RefreshableOnComponent = {
     val reformatBox = new NonFocusableCheckBox("Reformat with scalariform")
     disableWhenDumb(project, reformatBox, "Impossible until indices are up-to-date")
@@ -29,19 +32,19 @@ class ScalariformReformatCheckingHandler(project: Project, projectPanel: Checkin
       }
 
       override def saveState(): Unit = {
-        getSettings.REFORMAT_BEFORE_PROJECT_COMMIT = reformatBox.isSelected
+        getSettings.setValue(REFORMAT_BEFORE_PROJECT_COMMIT, reformatBox.isSelected)
       }
 
       override def restoreState(): Unit = {
-        reformatBox.setSelected(getSettings.REFORMAT_BEFORE_PROJECT_COMMIT)
+        reformatBox.setSelected(getSettings.getBoolean(REFORMAT_BEFORE_PROJECT_COMMIT))
       }
     }
   }
 
   override def runCheckinHandlers(finishAction: _root_.java.lang.Runnable): Unit = {
-    val configuration = VcsConfiguration.getInstance(project)
+    val shouldReformatBeforeCheckin = getSettings.getBoolean(REFORMAT_BEFORE_PROJECT_COMMIT)
 
-    if (configuration.REFORMAT_BEFORE_PROJECT_COMMIT && !DumbService.isDumb(project)) {
+    if (shouldReformatBeforeCheckin && !DumbService.isDumb(project)) {
       val maybePreferences = Preferences.fromProject(project.getBasePath)
       val filesToFormat = ChangeListManager.getInstance(project)
         .getAffectedFiles
@@ -57,7 +60,7 @@ class ScalariformReformatCheckingHandler(project: Project, projectPanel: Checkin
     }.run()
   }
 
-  protected def getSettings: VcsConfiguration = VcsConfiguration.getInstance(project)
+  protected def getSettings: PropertiesComponent = PropertiesComponent.getInstance(project)
 
   private def disableWhenDumb(project: Project, checkBox: JCheckBox, tooltip: String): Unit = {
     val dumb = DumbService.isDumb(project)
